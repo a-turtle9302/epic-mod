4d49fca5c6f78d9e00365606918e58ad53406dc3
turtle man <6jaybesup@gmail.com>
2026-03-07 12:43:08 -0500

    make the turtle a texture in inventory

--- src/main/java/com/meiscoolx2/epicmod/EpicMod.java
+++ src/main/java/com/meiscoolx2/epicmod/EpicMod.java
@@ -16,58 +16,58 @@ import java.io.FileWriter;
 import static net.fabricmc.fabric.impl.resource.pack.ModPackResourcesUtil.GSON;
 
 public class EpicMod implements ModInitializer {
-	public static final String MOD_ID = "epicmod";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "epicmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("who up epicing they mod");
-		ModItems.registerModItems();
-		ModBlocks.registerModBlocks();
+    @Override
+    public void onInitialize() {
+        LOGGER.info("who up epicing they mod");
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 
-		FuelRegistryEvents.BUILD.register((builder, context) -> {
-			builder.add(ModItems.SUSPICIOUS_SUBSTANCE, 600);
-		});
+        FuelRegistryEvents.BUILD.register((builder, context) -> {
+            builder.add(ModItems.SUSPICIOUS_SUBSTANCE, 600);
+        });
 
-		// HEY STACKOVERFLOW HOW DO I MAKE A JSON FILE
-		try {
-			File configFile = new File("config/attributefix/minecraft/attack_knockback.json");
+        // HEY STACKOVERFLOW HOW DO I MAKE A JSON FILE
+        try {
+            File configFile = new File("config/attributefix/minecraft/attack_knockback.json");
 
-			// Create new JSON structure
-			JsonObject root = new JsonObject();
+            // Create new JSON structure
+            JsonObject root = new JsonObject();
 
-			JsonObject modifyRange = new JsonObject();
-			modifyRange.addProperty("//", "Determines if the range for the attribute should be modified or not.");
-			modifyRange.addProperty("//default", false);
-			modifyRange.addProperty("value", true);
+            JsonObject modifyRange = new JsonObject();
+            modifyRange.addProperty("//", "Determines if the range for the attribute should be modified or not.");
+            modifyRange.addProperty("//default", false);
+            modifyRange.addProperty("value", true);
 
-			JsonObject min = new JsonObject();
-			min.addProperty("//", "The lowest possible value for the attribute.");
-			min.addProperty("//default", 0.0);
-			min.addProperty("value", 0.0);
+            JsonObject min = new JsonObject();
+            min.addProperty("//", "The lowest possible value for the attribute.");
+            min.addProperty("//default", 0.0);
+            min.addProperty("value", 0.0);
 
-			JsonObject max = new JsonObject();
-			max.addProperty("//", "The highest possible value for the attribute.");
-			max.addProperty("//default", 5.0);
-			max.addProperty("value", 1000.0);
+            JsonObject max = new JsonObject();
+            max.addProperty("//", "The highest possible value for the attribute.");
+            max.addProperty("//default", 5.0);
+            max.addProperty("value", 1000.0);
 
-			// Add fields to root
-			root.add("modify_range", modifyRange);
-			root.add("min", min);
-			root.add("max", max);
+            // Add fields to root
+            root.add("modify_range", modifyRange);
+            root.add("min", min);
+            root.add("max", max);
 
-			// Ensure parent directories exist
-			configFile.getParentFile().mkdirs();
+            // Ensure parent directories exist
+            configFile.getParentFile().mkdirs();
 
-			// Save to disk
-			try (FileWriter writer = new FileWriter(configFile)) {
-				GSON.toJson(root, writer);
-			}
+            // Save to disk
+            try (FileWriter writer = new FileWriter(configFile)) {
+                GSON.toJson(root, writer);
+            }
 
-			System.out.println("AttributeFix attack_knockback config auto-created/overwritten!");
+            System.out.println("AttributeFix attack_knockback config auto-created/overwritten!");
 
-		} catch (Exception e) {
-			throw new RuntimeException("Failed to auto-create AttributeFix attack_knockback config", e);
-		}
-	}
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to auto-create AttributeFix attack_knockback config", e);
+        }
+    }
 }
